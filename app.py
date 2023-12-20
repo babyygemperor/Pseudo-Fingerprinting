1c0a8b7467f819f73395ee0868dd40debee79707
Aamin Gem <gem.aamin@gmail.com>
2023-12-20 23:35:13 +0100

    Init

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, jsonify, render_template
+import json
+
+app = Flask(__name__)
+
+
+@app.after_request
+def add_headers(response):
+    """
+    Decorator function to add headers to the response.
+    Allows cross-origin resource sharing (CORS) by setting the 'Access-Control-Allow-Origin',
+    'Access-Control-Allow-Methods', and 'Access-Control-Allow-Headers' headers.
+    """
+    response.headers['Access-Control-Allow-Origin'] = '*'  # Allow requests from any origin
+    response.headers['Access-Control-Allow-Methods'] = 'POST, OPTIONS'  # Allow POST and OPTIONS methods
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type'  # Allow 'Content-Type' header
+    return response
+
+
+@app.route('/')
+def home():
+   return render_template('index.html')
+
+@app.route('/collect', methods=['POST'])
+def collect_fingerprint():
+    data = request.json
+    json_str = json.dumps(data, indent=2)
+    print("Received fingerprint data:", json_str)
+
+    return jsonify({"status": "success", "message": "Data received"})
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
